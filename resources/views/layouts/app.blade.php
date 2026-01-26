7af931797d5c310aca2db216ab4020c5ca58440b
Diego1002 <diegoaugusto.garciaa15@gmail.com>
2026-01-26 15:15:24 -0300

    UPDATE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <title>@yield('title', 'Sistema de controle')</title>
 
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/home.css') }}">
 </head>
 <body>
 
