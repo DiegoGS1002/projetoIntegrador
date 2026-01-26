5bbaf8d16cf240eb85f2116db02d0d9009282f2a
Diego1002 <diegoaugusto.garciaa15@gmail.com>
2026-01-26 15:06:04 -0300

    UPDATE REST

--- routes/api.php
+++ routes/api.php
@@ -6,32 +6,34 @@ use App\Http\Controllers\Api\ProductSupplierApiController;
 use App\Http\Controllers\Api\ClientApiController;
 use Illuminate\Support\Facades\Route;
 
-// Products CRUD
-Route::get('/products', [ProductApiController::class, 'index']);
-Route::post('/products', [ProductApiController::class, 'store']);
-Route::get('/products/{product}', [ProductApiController::class, 'show']);
-Route::put('/products/{product}', [ProductApiController::class, 'update']);
-Route::patch('/products/{product}', [ProductApiController::class, 'update']);
-Route::delete('/products/{product}', [ProductApiController::class, 'destroy']);
+Route::middleware('api')->group(function () {
+    // Products
+    Route::get('/products', [ProductApiController::class, 'index']);
+    Route::post('/products', [ProductApiController::class, 'store']);
+    Route::get('/products/{product}', [ProductApiController::class, 'show']);
+    Route::put('/products/{product}', [ProductApiController::class, 'update']);
+    Route::patch('/products/{product}', [ProductApiController::class, 'update']);
+    Route::delete('/products/{product}', [ProductApiController::class, 'destroy']);
 
-// Suppliers CRUD
-Route::get('/suppliers', [SupplierApiController::class, 'index']);
-Route::post('/suppliers', [SupplierApiController::class, 'store']);
-Route::get('/suppliers/{supplier}', [SupplierApiController::class, 'show']);
-Route::put('/suppliers/{supplier}', [SupplierApiController::class, 'update']);
-Route::patch('/suppliers/{supplier}', [SupplierApiController::class, 'update']);
-Route::delete('/suppliers/{supplier}', [SupplierApiController::class, 'destroy']);
+    // Suppliers
+    Route::get('/suppliers', [SupplierApiController::class, 'index']);
+    Route::post('/suppliers', [SupplierApiController::class, 'store']);
+    Route::get('/suppliers/{supplier}', [SupplierApiController::class, 'show']);
+    Route::put('/suppliers/{supplier}', [SupplierApiController::class, 'update']);
+    Route::patch('/suppliers/{supplier}', [SupplierApiController::class, 'update']);
+    Route::delete('/suppliers/{supplier}', [SupplierApiController::class, 'destroy']);
 
-// Clients CRUD
-Route::get('/clients', [ClientApiController::class, 'index']);
-Route::post('/clients', [ClientApiController::class, 'store']);
-Route::get('/clients/{client}', [ClientApiController::class, 'show']);
-Route::put('/clients/{client}', [ClientApiController::class, 'update']);
-Route::patch('/clients/{client}', [ClientApiController::class, 'update']);
-Route::delete('/clients/{client}', [ClientApiController::class, 'destroy']);
+    // Clients
+    Route::get('/clients', [ClientApiController::class, 'index']);
+    Route::post('/clients', [ClientApiController::class, 'store']);
+    Route::get('/clients/{client}', [ClientApiController::class, 'show']);
+    Route::put('/clients/{client}', [ClientApiController::class, 'update']);
+    Route::patch('/clients/{client}', [ClientApiController::class, 'update']);
+    Route::delete('/clients/{client}', [ClientApiController::class, 'destroy']);
 
-// Product Suppliers (N:N relationship)
-Route::get('/products/{product}/suppliers', [ProductSupplierApiController::class, 'index']);
-Route::post('/products/{product}/suppliers', [ProductSupplierApiController::class, 'attach']);
-Route::delete('/products/{product}/suppliers/{supplier}', [ProductSupplierApiController::class, 'detach']);
+    // Product x Supplier
+    Route::get('/products/{product}/suppliers', [ProductSupplierApiController::class, 'index']);
+    Route::post('/products/{product}/suppliers', [ProductSupplierApiController::class, 'attach']);
+    Route::delete('/products/{product}/suppliers/{supplier}', [ProductSupplierApiController::class, 'detach']);
+});
 
